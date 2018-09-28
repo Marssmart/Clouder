d27012a5f30d8ba28b8fbe727d7f093ae5a114ba
jan.srnicek <jan.srnicek@globallogic.com>
2018-09-28 12:04:40 +0200

    Initial spring structure

---
+++ src/main/java/org/deer/clouder/ClouderApplication.java
@@ -0,0 +1,12 @@
+package org.deer.clouder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClouderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClouderApplication.class, args);
+	}
+}
